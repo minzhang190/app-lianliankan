7d2ce849ecfe21f9a8bbcfa83d82e457beab7005
minzhang190 <minzhang190@gmail.com>
2020-04-21 10:38:16 +0000

    Adaption

---
+++ public/default.js
@@ -0,0 +1,17 @@
+var props = {
+    width: 800,
+    height: 600,
+    columns: 8,
+    rows: 6,
+    range: 9,
+    use: -1,
+    zeroThrottle: 1,
+    maxCellSize: 50,
+    cellMargin: 2,
+    gameTimeLimit: 200,
+    durationIncrease: 5,
+    suggestTimes: 99,
+    shuffleTimes: 99,
+    shuffleOnStuck: true,
+    matching: true
+};
--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import Game from './Game'
 import './style.css'
 
 class App extends Component {
-  state = {}
+  state = {
+    cssLoaded: false,
+    dataJSLoaded: false,
+    dataCSSLoaded: false,
+    props: null
+  }
 
   componentDidMount() {
     const el = document.querySelector('link[rel="stylesheet"]')
@@ -14,12 +19,58 @@ class App extends Component {
         cssLoaded: true
       })
     )
+
+    const data = new URLSearchParams(window.location.search).get('data') || 'default';
+    const app = this, audio = [null];
+
+    const onClick = (point, value, selected) => {
+        if (selected.length === 1 && audio[Math.abs(value)]) {
+            audio[Math.abs(value)].play();
+        }
+    };
+
+    const onLinked = (selected, v1, v2) => {
+        if (audio[Math.abs(v1)]) {
+            audio[Math.abs(v1)].play();
+        }
+    };
+
+    window.props = {};
+    window.loadSound = (array) => {
+        array.forEach(path => {
+            const currentAudio = new Audio(`data/${data}/${path}`);
+            audio.push(currentAudio);
+        });
+    };
+
+    const styleData = document.createElement('link');
+    styleData.rel = 'stylesheet';
+    styleData.href = `data/${data}/style.css`;
+    styleData.addEventListener('load', () => {
+        app.setState({ dataCSSLoaded: true });
+    });
+    document.head.appendChild(styleData);
+
+    const scriptDefault = document.createElement('script');
+    scriptDefault.src = 'default.js';
+    scriptDefault.async = false;
+    document.body.appendChild(scriptDefault);
+
+    const scriptData = document.createElement('script')
+    scriptData.src = `data/${data}/script.js`;
+    scriptData.async = false;
+    scriptData.addEventListener('load', () => {
+        window.props.onClick = onClick;
+        window.props.onLinked = onLinked;
+        app.setState({ dataJSLoaded: true, props: window.props });
+    });
+    document.body.appendChild(scriptData);
   }
 
   render() {
-    const { cssLoaded } = this.state
-    return cssLoaded ? (
-      <Game/>
+    const { cssLoaded, dataJSLoaded, dataCSSLoaded, props } = this.state
+    return (cssLoaded && dataJSLoaded && dataCSSLoaded) ? (
+      <Game {...props} />
     ) : null;
   }
 }
